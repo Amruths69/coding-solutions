f53488b32e6fb7720208e12b25512b43bb586e5c
Amruths69 <kit28.24bec009@gmail.com>
2026-06-30 19:18:08 +0530

    ✅ Sqrt(x) | Easy | Java | Runtime: 0 ms | Memory: 41.9 MB | 2026-06-30

--- leetcode/easy/0069-sqrtx/solution.java
+++ leetcode/easy/0069-sqrtx/solution.java
@@ -1,11 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
         int m=0;
+        int max=0;
         for(int i=1;i<x;i++){
-            if(i*i==x){
+            if(i*i<=x){
             m= i;
-           break;
+           
             }
+            
+
+
+            
         }
         return m;
         
