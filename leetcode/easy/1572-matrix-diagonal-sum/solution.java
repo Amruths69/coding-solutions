17d2eae2be9cc36d27cfe14583b886af2044f852
Amruths69 <kit28.24bec009@gmail.com>
2026-06-30 13:06:07 +0530

    ✅ Matrix Diagonal Sum | Easy | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-06-30

---
+++ leetcode/easy/1572-matrix-diagonal-sum/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+
+        int n = mat.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // primary diagonal
+            sum += mat[i][i];
+
+            // secondary diagonal
+            sum += mat[i][n - 1 - i];
+        }
+
+        // if matrix size is odd,
+        // center element counted twice
+        if (n % 2 == 1) {
+            sum -= mat[n / 2][n / 2];
+        }
+
+        return sum;
+    }
+}
