922927d71ddef5629c9089fb11dfe1cb6e3b3e4f
Amruths69 <kit28.24bec009@gmail.com>
2026-07-01 06:34:17 +0530

    ✅ Trionic Array I | Easy | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-07-01

--- leetcode/easy/3637-trionic-array-i/solution.java
+++ leetcode/easy/3637-trionic-array-i/solution.java
@@ -1,22 +1,38 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-        int g=nums[0];
-        int f=0;
-        for(int i=1;i<nums.length;i++){
-            if(g<nums[i]){
-                g=nums[i];
+        int n = nums.length;
+        int i = 0;
 
-            }else if(g>nums[i]){
-                f=nums[i];
-
-            }
-
+        // increasing
+        while (i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
         }
-        if(g<f){
-            return true;
-        }
-        return false;
 
-        
+        // must have at least one increase
+        if (i == 0 || i == n - 1) {
+            return false;
+        }
+
+        // decreasing
+        int down = i;
+
+        while (i + 1 < n && nums[i] > nums[i + 1]) {
+            i++;
+        }
+
+        // must have at least one decrease
+        if (i == down || i == n - 1) {
+            return false;
+        }
+
+        // increasing again
+        int up = i;
+
+        while (i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+
+        // must end properly
+        return i == n - 1 && up != i;
     }
 }
