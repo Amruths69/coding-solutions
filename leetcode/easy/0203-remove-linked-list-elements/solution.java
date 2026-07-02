394dd8b2f564c174da20d5d9206fd8f2f955830d
Amruth S <kit28.24bec009@gmail.com>
2026-07-02 14:17:38 +0530

    ✅ Remove Linked List Elements | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-07-02

---
+++ leetcode/easy/0203-remove-linked-list-elements/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode t=head;
+        while(t!=null && t.next!=null){
+            if(t.val==val){
+                t.next=t.next.next;
+            }else{
+                t=t.next;
+            }
+        }
+        return head;
+        
+    }
+}
