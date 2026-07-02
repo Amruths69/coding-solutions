b4c00912aa47b926978a3a7c3d390a1262f11f1c
Amruths69 <kit28.24bec009@gmail.com>
2026-07-02 13:44:47 +0530

    ✅ Reverse Linked List | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-07-02

---
+++ leetcode/easy/0206-reverse-linked-list/solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode p=null;
+        ListNode c=head;
+        while(c!=null){
+            ListNode next=c.next;
+            p=c;
+            c=next;
+        }
+        return p;
+    }
+}
