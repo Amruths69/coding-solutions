ac522c7039212c89c0eb8495d11fdf72c9acbfc4
Amruth S <kit28.24bec009@gmail.com>
2026-07-03 19:34:06 +0530

    ✅ AGEING - Rating 298 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-07-03

---
+++ codechef/easy/AGEING---Rating-298/solution.cpp
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner a=new Scanner(System.in);
+		int t=a.nextInt();
+		while(t-->0){
+		    int j=a.nextInt();
+		    int to=a.nextInt();
+		    if(to<j||to==j){
+		        System.out.println("No");
+		    }else{
+		        System.out.println("Yes");
+		    }
+		}
+
+	}
+}
