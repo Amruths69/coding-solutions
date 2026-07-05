bbad214f7e31021007cf0f67ae2759d5614ef73d
Amruth S <kit28.24bec009@gmail.com>
2026-07-05 08:31:53 +0530

    ✅ Q1. Sum of Integers with Maximum Digit Range | Easy | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-07-05

---
+++ leetcode/easy/sum-of-integers-with-maximum-digit-range/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxDigitRange(int[] nums) {
+        
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            
+                int h=nums[i];
+                int max=0;
+                int min=0;
+                while(h>0){
+                    int d=h%10;
+                    if(d>max){
+                        max=d;
+                    }if(d<min){
+                        min=d;
+                    }
+                    h=h/10;
+                }
+            ans[i]=max-min;
+            
+            
+            
+        }
+        int r=0;
+        
+        for(int i=0;i<ans.length;i++){
+            
+            if(ans[i]>r){
+                r=ans[i];
+                    
+            }
+            
+        }
+        int s=0;
+        for(int i=0;i<nums.length;i++){
+            if(ans[i]==r){
+                s+=nums[i];
+            }
+        }
+        return s;
+        
+    }
+}
