2a96cb90ec61081cc6308c41ab4b238826f3dbaa
Amruth S <kit28.24bec009@gmail.com>
2026-07-13 09:51:42 +0530

    ✅ Running Sum of 1d Array | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-07-13

--- leetcode/easy/1480-running-sum-of-1d-array/solution.java
+++ leetcode/easy/1480-running-sum-of-1d-array/solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         int[] ans=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
+        ans[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            ans[i]=ans[i-1]+nums[i];
             
-            for(int j=0;j<=i;j++){
-                
-                ans[i]+=nums[j];
-
-            }
+            
         }
         return ans;
     }
