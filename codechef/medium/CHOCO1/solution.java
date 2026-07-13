22dd2a96fedcfcadacfecea1274254fd6ee17677
Amruth S <kit28.24bec009@gmail.com>
2026-07-13 21:36:20 +0530

    ✅ CHOCO1 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-13

--- codechef/medium/CHOCO1/solution.java
+++ codechef/medium/CHOCO1/solution.java
@@ -6,7 +6,22 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner a=new Scanner(System.in);
+		int t=a.nextInt();
+		while(t-->0){
+		    int l=a.nextInt();
+		    int b=a.nextInt();
+		    int max=0;
+		    int maxxy=0;
+		    for(int i=0;i<b+l;i++){
+		        if(l%i==0 && b%i==0){
+		            maxxy=i;
+		        }
+		        max=Math.max(max,maxxy);
+		        
+		    }
+		    System.out.println(max);
+		}
 
 	}
 }
