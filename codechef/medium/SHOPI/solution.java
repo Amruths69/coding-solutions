b2a7fd4e8a5e32a72f034d9cd8d6eb9de93c2754
Amruth S <kit28.24bec009@gmail.com>
2026-07-13 21:53:29 +0530

    ✅ SHOPI | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-13

--- codechef/medium/SHOPI/solution.java
+++ codechef/medium/SHOPI/solution.java
@@ -6,7 +6,25 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	    Scanner a= new Scanner(System.in);
+	    int t=a.nextInt();
+	    while(t-->0){
+	        int n=a.nextInt();
+	        int k=a.nextInt();
+	        int min=Math.min(n,n-k);
+	        int sm=0;
+	        int to=0;
+	        int[] arr=new int[n];
+	        for(int j=0;j<n;j++){
+	            arr[j]=a.nextInt(j);
+	        }
+	        for(int i=0;i<min;i++){
+	             sm+=arr[i];
+	        }for(int i=0;i<n;i++){
+	            to+=arr[i];
+	        }
+	        System.out.println(to-sm);
+	    }
 
 	}
 }
