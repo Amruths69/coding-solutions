77d1383ffc253a25a888f1a400af31aacde1c4b6
Amruth S <kit28.24bec009@gmail.com>
2026-07-13 21:29:35 +0530

    ✅ SALARY1 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-13

---
+++ codechef/medium/SALARY1/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner a=new Scanner(System.in);
+		int s=a.nextInt();
+		int b=a.nextInt();
+		int r=a.nextInt();
+		System.out.println((b*r)+s);
+
+	}
+}
