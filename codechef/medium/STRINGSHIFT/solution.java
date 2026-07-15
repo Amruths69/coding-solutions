57fa9a8f3cc099140f1a40d231c9bfe05724498d
Amruth S <kit28.24bec009@gmail.com>
2026-07-15 21:39:30 +0530

    ✅ STRINGSHIFT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/STRINGSHIFT/solution.java
+++ codechef/medium/STRINGSHIFT/solution.java
@@ -2,11 +2,35 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+import java.util.*;
 
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int t = s.nextInt();
+
+        while (t-- > 0) {
+            int n = s.nextInt();
+            char[] x = s.next().toCharArray();
+
+            int a = -1;
+
+            for (int i = 0; i < n; i++) {
+                if (x[i] == 'z') {
+                    a = i;
+                    break;
+                }
+            }
+
+            if (a != -1) {
+                while (a < n && x[a] == 'z') {
+                    x[a] = 'a';
+                    a++;
+                }
+            }
+
+            System.out.println(new String(x));
+        }
+    }
 }
