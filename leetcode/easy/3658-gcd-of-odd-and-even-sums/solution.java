009b5b243fc20d0fc507c3382733d7d112d41d9b
Amruth S <kit28.24bec009@gmail.com>
2026-07-15 11:12:13 +0530

    ✅ GCD of Odd and Even Sums | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42.5 MB | 2026-07-15

--- leetcode/easy/3658-gcd-of-odd-and-even-sums/solution.java
+++ leetcode/easy/3658-gcd-of-odd-and-even-sums/solution.java
@@ -1,28 +1,6 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int se=0;
-        int so=0;
-        if(n==1)
-        return 1;
-        for(int i=1;i<2*n;i++){
-            if(i%2==0){
-                se+=i;
-
-            }else if(i%2!=0){
-                so+=i;
-            }
-        }
-        int max=0;
-        int c=0;
-        
-        for(int i=1;i<so;i++){
-            if(so%i==0 && se%i==0){
-                c=i;
-            }
-            max=Math.max(c,max);
-
-        }
-        return max;
+        return n;
         
     }
 }
