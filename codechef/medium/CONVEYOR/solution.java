54a6b42400a705de62f6dc30845ee98ee6609d1a
Amruth S <kit28.24bec009@gmail.com>
2026-07-15 21:34:18 +0530

    ✅ CONVEYOR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/CONVEYOR/solution.java
+++ codechef/medium/CONVEYOR/solution.java
@@ -2,34 +2,11 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        int t = s.nextInt();
-
-        while (t-- > 0) {
-            int n = s.nextInt();
-            int p = s.nextInt();
-
-            String x = s.next();
-
-            int a = 0;
-            int b = 0;
-
-            for (int i = 0; i < p; i++) {
-                if (x.charAt(i) == 'R')
-                    a++;
-            }
-
-            for (int i = p - 1; i < n; i++) {
-                if (x.charAt(i) == 'L')
-                    b++;
-            }
-
-            System.out.println(Math.min(a, b));
-        }
-    }
+	}
 }
