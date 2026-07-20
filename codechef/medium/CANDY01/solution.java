f160e476d98459ff8614632708f29258a77024cb
Amruth S <kit28.24bec009@gmail.com>
2026-07-20 20:43:38 +0530

    ✅ CANDY01 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-20

--- codechef/medium/CANDY01/solution.java
+++ codechef/medium/CANDY01/solution.java
@@ -6,7 +6,16 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
-
+		Scanner a=new Scanner(System.in);
+		int n=a.nextInt();
+		int c=a.nextInt();
+		int g=0;
+		for(int i=0;i<n;i++){
+		    g+=a.nextInt();
+		}
+        if(g<=0)
+        System.out.println("yes");
+        else
+        System.out.println("no");
 	}
 }
