0b90bfbbc3abffc7bd623ef23b576e12bc626f3c
Amruth S <kit28.24bec009@gmail.com>
2026-07-20 21:11:11 +0530

    ✅ Shift 2D Grid | Easy | Java | Runtime: 5 ms (83.45%) | Memory: 46.9 MB | 2026-07-20

---
+++ leetcode/easy/1260-shift-2d-grid/solution.java
@@ -0,0 +1,38 @@
+//daily-dump
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+
+        k %= total;
+
+        int[][] ans = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int idx = i * n + j;
+                int next = (idx + k) % total;
+
+                int r = next / n;
+                int c = next % n;
+
+                ans[r][c] = grid[i][j];
+            }
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(ans[i][j]);
+            }
+            res.add(row);
+        }
+
+        return res;
+    }
+}
