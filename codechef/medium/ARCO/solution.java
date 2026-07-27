2dad287068aa583ed2a267494f9139d5f5bc2db1
Amruth S <kit28.24bec009@gmail.com>
2026-07-27 21:37:06 +0530

    ✅ ARCO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/ARCO/solution.java
+++ codechef/medium/ARCO/solution.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner a=new Scanner(System.in);
+        int n=a.nextInt();
+        ArrayList<Integer> arr= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            arr.add(a.nextInt());
+            
+        }
+        for(int i=1;i<n;i++){
+            if(arr.get(i-1)==arr.get(i)||arr.get(i+1)==arr.get(i)){
+                arr.remove(arr.get(i));
+                
+            }
+        }
+        System.out.println(arr.size());
         
     }
 }
