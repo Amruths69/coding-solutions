d88211d1d871dafd909bb149f64bbe680858316b
Amruth S <kit28.24bec009@gmail.com>
2026-07-27 21:19:43 +0530

    ✅ UTNL | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-27

---
+++ codechef/medium/UTNL/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner a=new Scanner(System.in);
+		int x=a.nextInt();
+		int y=a.nextInt();
+		int g=x-y;
+		if(g>0){
+		    System.out.println(g);
+		}else if(g<=0){
+		    System.out.println("UNLOCKED");
+		    
+		}
+
+	}
+}
