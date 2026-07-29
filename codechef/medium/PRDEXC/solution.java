7b71246fb7f6659c73be90ae951250dce5b44741
Amruth S <kit28.24bec009@gmail.com>
2026-07-29 20:48:00 +0530

    ✅ PRDEXC | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/PRDEXC/solution.java
+++ codechef/medium/PRDEXC/solution.java
@@ -6,7 +6,27 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner a=new Scanner(System.in);
+		int t=a.nextInt();
+		while(t-->0){
+		    int x=a.nextInt();
+		    int y=a.nextInt();
+		    int p=a.nextInt();
+		    int c=a.nextInt();
+		    if((x*y)>=p){
+		        System.out.println(0);
+		    }else{
+		        while((x*y)<p){
+		            x++;
+		            c++;
+		            if((x*y)>=p){
+		                System.out.println(c);
+		                
+		                
+		            }
+		        }
+		    }
+		}
 
 	}
 }
