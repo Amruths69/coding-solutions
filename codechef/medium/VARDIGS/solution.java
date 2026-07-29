49cb38e7e1406d8ceba8231a0bb0ac1e0568294d
Amruth S <kit28.24bec009@gmail.com>
2026-07-29 20:20:50 +0530

    ✅ VARDIGS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-29

---
+++ codechef/medium/VARDIGS/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner a=new Scanner(System.in);
+		int x=a.nextInt();
+		String f=Integer.toString(x);
+		if(f.charAt(0)!=f.charAt(1)){
+		    System.out.println("Yes");
+		    
+		}else{
+		    System.out.println("No");
+		}
+
+	}
+}
