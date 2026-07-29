6e394e59cb28562d20024e2d3e339b1033f0dd71
Amruth S <kit28.24bec009@gmail.com>
2026-07-29 20:47:39 +0530

    ✅ GPUBUY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/GPUBUY/solution.java
+++ codechef/medium/GPUBUY/solution.java
@@ -6,29 +6,7 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner a=new Scanner(System.in);
-		int t=a.nextInt();
-		while(t-->0){
-		    int z=a.nextInt();
-		    int x=a.nextInt();
-		    int y=a.nextInt();
-		    int c=0;
-		    int c1=0;
-		    if(x>=y){
-		        System.out.println(-1);
-		        
-		    }else{
-		    
-		    while(c<z){
-		        c+=y;
-		        z+=x;
-		        c1++;
-		        if(c>=z){
-		            System.out.println(c1);
-		            
-		        }}
-		    }
-		}
+		// your code goes here
 
 	}
 }
