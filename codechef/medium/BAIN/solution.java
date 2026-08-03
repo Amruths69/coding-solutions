5150d7654a05f64da6d3957a4125ea4547419639
Amruth S <kit28.24bec009@gmail.com>
2026-08-03 20:22:16 +0530

    ✅ BAIN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-03

---
+++ codechef/medium/BAIN/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner a=new Scanner(System.in);
+		int[] arr=new int[4];
+		for(int i=0;i<4;i++){
+		    arr[i]=a.nextInt();
+		    
+		}
+		for(int i=0;i<4;i++){
+		    for(int j=0;j<4;j++){
+		        if(arr[])
+		    }
+		}
+
+	}
+}
