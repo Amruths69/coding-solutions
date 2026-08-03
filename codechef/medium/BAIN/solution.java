9d0676bd2711dd6103e420afe587d9d8a4b289fa
Amruth S <kit28.24bec009@gmail.com>
2026-08-03 20:27:17 +0530

    ✅ BAIN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/BAIN/solution.java
+++ codechef/medium/BAIN/solution.java
@@ -13,10 +13,27 @@ class Codechef
 		    arr[i]=a.nextInt();
 		    
 		}
-		for(int i=0;i<4;i++){
-		    for(int j=0;j<4;j++){
-		        if(arr[])
-		    }
+		if(
+		    arr[0] == 0 || arr[1] == 0 || arr[2] == 0 || arr[3] == 0 ||
+
+		    arr[0] + arr[1] == 0 ||
+		    arr[0] + arr[2] == 0 ||
+		    arr[0] + arr[3] == 0 ||
+		    arr[1] + arr[2] == 0 ||
+		    arr[1] + arr[3] == 0 ||
+		    arr[2] + arr[3] == 0 ||
+
+		    arr[0] + arr[1] + arr[2] == 0 ||
+		    arr[0] + arr[1] + arr[3] == 0 ||
+		    arr[0] + arr[2] + arr[3] == 0 ||
+		    arr[1] + arr[2] + arr[3] == 0 ||
+
+		    arr[0] + arr[1] + arr[2] + arr[3] == 0
+		){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
 		}
 
 	}
