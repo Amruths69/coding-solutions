fd8e767077478cc9c67ba0642634b6dc81899254
Amruth S <kit28.24bec009@gmail.com>
2026-08-03 20:47:53 +0530

    ✅ CLSC | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/CLSC/solution.java
+++ codechef/medium/CLSC/solution.java
@@ -8,7 +8,21 @@ class Codechef
 	{
 		Scanner a= new Scanner(System.in);
 		int n=a.nextInt();
-		
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=a.nextInt();
+		}
+		Arrays.sort(arr);
+	    int bn=Integer.MAX_VALUE;
+		for(int i=1;i<n;i++){
+		    int v=arr[i]-arr[i-1];
+		    bn=Math.min(bn,v);
+		    
+		    
+		   
+		    
+		}
+		System.out.println(bn);
 
 	}
 }
