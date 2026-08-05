811b6a8fa3044b4d825e84365de962c25c00ffb5
Amruth S <kit28.24bec009@gmail.com>
2026-08-05 21:05:44 +0530

    ✅ SUBCOST7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/SUBCOST7/solution.java
+++ codechef/medium/SUBCOST7/solution.java
@@ -6,7 +6,17 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
-
+		Scanner a=new Scanner(System.in);
+		int t=a.nextInt();
+		while(t-->0){
+		int n=a.nextInt();
+		int x=a.nextInt();
+		int y=a.nextInt();
+		if(n<=3)
+		System.out.println(x*n);
+		else{
+		    System.out.println(((3*x)+(n-3)*y));
+		}
+}
 	}
 }
