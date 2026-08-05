3a6b10b30594d4c28dcd0b71e9884489e1b46518
Amruth S <kit28.24bec009@gmail.com>
2026-08-05 21:33:30 +0530

    ✅ BISHOPMV | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/BISHOPMV/solution.java
+++ codechef/medium/BISHOPMV/solution.java
@@ -6,7 +6,21 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	Scanner a=new Scanner(System.in);
+	int t=a.nextInt();
+	while(t-->0){
+	    int x1=a.nextInt();
+	    int y1=a.nextInt();
+	    int x2=a.nextInt();
+	    int y2=a.nextInt();
+	    if(x1+y1==x2+y2||x1-y1==x2-y2){
+	        System.out.println(1);
+	    }
+	    else if((x2+y2)%2!=(x1+y1)%2)
+	    System.out.println(-1);
+	    else
+	    System.out.println(2);
+	}
 
 	}
 }
