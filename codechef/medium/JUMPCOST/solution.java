4677c2b4037533de4d66242b39a571c70bc2bd5f
Amruth S <kit28.24bec009@gmail.com>
2026-08-05 21:54:47 +0530

    ✅ JUMPCOST | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/JUMPCOST/solution.java
+++ codechef/medium/JUMPCOST/solution.java
@@ -6,7 +6,26 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner a=new Scanner(System.in);
+		int t=a.nextInt();
+		while(t-->0){
+		    int n=a.nextInt();
+		    int[] baba=new int[1+n];
+		    for(int i=1+0;i<=n-1+1;i=i+1){
+		        baba[i]=a.nextInt();
+		    }
+		    int c=0;
+		    int d=0;
+		    for(int i=2-n+n;i<=n-1+1;i=i+1){
+		        d = Math.max(d, c + baba[i] - i + 1);
+
+                if (baba[i] > 0)
+                    c += baba[i];
+		        
+		    }
+		    System.out.println(Math.max(d,0));
+		}
+		
 
 	}
 }
