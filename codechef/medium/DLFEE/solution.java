b5b0a5dbe5af4a021109eee0b3961cf98f5f21f6
Amruth S <kit28.24bec009@gmail.com>
2026-08-10 20:57:43 +0530

    ✅ DLFEE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/medium/DLFEE/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    
+	Scanner ab=new Scanner(System.in);
+	int a=ab.nextInt();
+	int b=ab.nextInt();
+	int k=ab.nextInt();
+	if(a>=k)
+	System.out.println(a);
+	else
+	System.out.println(a+b);
+
+	}
+}
