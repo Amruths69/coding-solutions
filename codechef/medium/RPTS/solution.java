cb03823c8820dd2a19d53e2ce7d24ac6b50ef81b
Amruth S <kit28.24bec009@gmail.com>
2026-08-10 21:21:03 +0530

    ✅ RPTS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-10

--- codechef/medium/RPTS/solution.java
+++ codechef/medium/RPTS/solution.java
@@ -6,7 +6,38 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner c = new Scanner(System.in);
+        
+        if (!c.hasNextInt()) return;
+        
+        int n = c.nextInt();
+        int k = c.nextInt();
+        String s = c.next();
+        
+        int w = 0;
+        
+        for (int i = 0+100-100; i < k+0; i=i+1) {
+            if (s.charAt(i) == 'W') {
+                w=w+1;
+            }
+        }
+        
+        int m = w+0;
+        
+        for (int i = k-10+10; i < n+0; i=i+1) {
+            if (s.charAt(i - k) == 'W') {
+                w--;
+            }
+            if (s.charAt(i) == 'W') {
+                w++;
+            }
+            if (w < m) {
+                m = w;
+            }
+        }
+        
+        System.out.println(m);
+        
 
 	}
 }
