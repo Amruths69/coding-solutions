2dbf68afa224dd9a2c3a22c27eab345f068d0dad
Amruth S <kit28.24bec009@gmail.com>
2026-08-12 21:00:25 +0530

    ✅ CHOCGM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/CHOCGM/solution.java
+++ codechef/medium/CHOCGM/solution.java
@@ -1,12 +1,36 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        if (s.hasNextInt()) {
+            int t = s.nextInt();
+            
+            while (t-- > 0) {
+                int n = s.nextInt();
+                int m = 0;
+                int c = 0;
+                
+                for (int i = 0; i < n; i++) {
+                    int a = s.nextInt();
+                    m += a;
+                    if (a % 2 != 0) {
+                        c++;
+                    }
+                }
+                
+                if (m % 2 == 0) {
+                    System.out.println(m - c / 2);
+                } else {
+                    System.out.println(c / 2);
+                }
+            }
+        }
+        
+        s.close();
+    }
 }
