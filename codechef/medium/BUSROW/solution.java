d42ee293bc7bac4481f4be16478d74c0794bdce5
Amruth S <kit28.24bec009@gmail.com>
2026-08-12 20:52:54 +0530

    ✅ BUSROW | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/BUSROW/solution.java
+++ codechef/medium/BUSROW/solution.java
@@ -2,11 +2,29 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
 
-	}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        if (s.hasNextInt()) {
+            int t = s.nextInt();
+            
+            while (t-- > 0) {
+                int n = s.nextInt();
+                int m = s.nextInt();
+                int x = s.nextInt();
+                
+                int r = (x - 1) / m + 1;
+                int f = r;
+                int b = n - r + 1;
+                int y = Math.min(f, b);
+                
+                System.out.println(y);
+            }
+        }
+        
+        
+    }
 }
