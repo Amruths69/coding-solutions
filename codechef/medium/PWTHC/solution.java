3639f35b0395b96e974bef765c10a3638cea9957
Amruth S <kit28.24bec009@gmail.com>
2026-08-17 21:06:17 +0530

    ✅ PWTHC | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/PWTHC/solution.java
+++ codechef/medium/PWTHC/solution.java
@@ -1,27 +1,12 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
 
-        if (scanner.hasNextInt()) {
-            int n = scanner.nextInt();
-            int k = scanner.nextInt();
-            int p = scanner.nextInt();
-            
-     
-            int totalChairs = n * k;
-            
-
-            if (totalChairs >= p) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
-        }
-   
-    }
+	}
 }
