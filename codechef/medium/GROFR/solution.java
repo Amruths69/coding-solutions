d91439b198e9c849728185944e9e1259d60e7810
Amruth S <kit28.24bec009@gmail.com>
2026-08-17 21:15:00 +0530

    ✅ GROFR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/GROFR/solution.java
+++ codechef/medium/GROFR/solution.java
@@ -1,1 +1,12 @@
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+
+	}
+}
