878dba40717ee16c458bc6dbb45e70a9d7c85d6f
Amruth S <kit28.24bec009@gmail.com>
2026-08-17 21:20:23 +0530

    ✅ GROFR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/GROFR/solution.java
+++ codechef/medium/GROFR/solution.java
@@ -6,7 +6,36 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		
+		import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            String s = scanner.next();
+            
+            int groupCount = 0;
+            boolean inGroup = false;
+            
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '1') {
+                    if (!inGroup) {
+                        groupCount++;
+                        inGroup = true;
+                    }
+                } else {
+                    inGroup = false;
+                }
+            }
+            
+            System.out.println(groupCount);
+        }
+        
+       
+    }
+}
 
 	}
 }
