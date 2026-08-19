180cd89dd684cae2740faa71021863dacaf450ed
Amruth S <kit28.24bec009@gmail.com>
2026-08-19 20:42:10 +0530

    ✅ IOI7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/IOI7/solution.java
+++ codechef/medium/IOI7/solution.java
@@ -6,7 +6,13 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	    
+	    Scanner a=new Scanner(System.in);
+	    int n=a.nextInt();
+	    int g=a.nextInt();
+	    if(n>=g)
+	    System.out.println("Yes");
+	    else
+	    System.out.println("No");
 
 	}
 }
