c587a51156cc65e650ce43dd4ed82456d7575f4a
Amruth S <kit28.24bec009@gmail.com>
2026-08-19 21:01:06 +0530

    ✅ ODDEVEN7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/ODDEVEN7/solution.java
+++ codechef/medium/ODDEVEN7/solution.java
@@ -2,11 +2,32 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+import java.util.*;
 
-	}
+public class Main {
+    public static void main(String[] x) {
+        Scanner s = new Scanner(System.in);
+        if (s.hasNextInt()) {
+            int t = s.nextInt();
+            while (t-- > 0) {
+                int n = s.nextInt();
+                int o = 0;
+                int e = 0;
+                
+                for (int i = 0; i < n; i++) {
+                    int v = s.nextInt();
+                    if (v % 2 == 0) {
+                        e++;
+                    } else {
+                        o++;
+                    }
+                }
+                
+                int c = Math.min(n, 2 * Math.min(o, e) + 1);
+                
+                System.out.println(c);
+            }
+        }
+        
+    }
 }
