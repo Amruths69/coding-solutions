37b0781dacdd2eaffd024c0868b4b8254577b70a
Amruth S <kit28.24bec009@gmail.com>
2026-08-19 21:03:25 +0530

    ✅ MRSWAP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/MRSWAP/solution.java
+++ codechef/medium/MRSWAP/solution.java
@@ -1,30 +1,12 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-import java.util.*;
 
-public class Main {
-    public static void main(String[] x) {
-        Scanner s = new Scanner(System.in);
-        if (s.hasNextInt()) {
-            int t = s.nextInt();
-            while (t-- > 0) {
-                int n = s.nextInt();
-                int m = n * 2;
-                int[] a = new int[m];
-                
-                for (int i = 0; i < m; i++) {
-                    a[i] = s.nextInt();
-                }
-                
-                int c = 0;
-                for (int i = 0; i < n; i++) {
-                    c += Math.max(a[i], a[m - 1 - i]);
-                }
-                
-                System.out.println(c);
-            }
-        }
-        s.close();
-    }
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
 }
