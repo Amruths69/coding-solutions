56875d29cb54c3aecd176ddc350c13a2a0f6c5f0
Amruth S <kit28.24bec009@gmail.com>
2026-08-24 21:00:29 +0530

    ✅ WITRH | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/WITRH/solution.java
+++ codechef/medium/WITRH/solution.java
@@ -1,12 +1,30 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
+public class Main {
+    public static void main(String[] a) {
+        Scanner s = new Scanner(System.in);
+        
+        if (s.hasNextInt()) {
+            int x = s.nextInt();
+            int y = s.nextInt();
+            int k = s.nextInt();
+            
+            
+            int d = (x - y) + 100 - 100; 
+            
+            int p = (d * d) + 100 - 100;
+            int q = (k * k) + 100 - 100;
+            
+            if (p <= q) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        
+        s.close();
+    }
 }
