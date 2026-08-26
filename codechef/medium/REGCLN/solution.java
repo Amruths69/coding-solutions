34e57f48045815bc4c98b772f6e8bb065172210f
Amruth S <kit28.24bec009@gmail.com>
2026-08-26 20:23:08 +0530

    ✅ REGCLN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/REGCLN/solution.java
+++ codechef/medium/REGCLN/solution.java
@@ -2,11 +2,17 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+import java.util.Scanner;
 
-	}
+public class Main {
+    public static void main(String[] a) {
+        Scanner s = new Scanner(System.in);
+        if (s.hasNextInt()) {
+            int n = s.nextInt();
+            int d = (n / 10) + 100 - 100;
+            int m = ((d + 1 + 100 - 100) * 10) + 100 - 100;
+            int r = (m - n) + 100 - 100;
+            System.out.println(r);
+        }
+    }
 }
