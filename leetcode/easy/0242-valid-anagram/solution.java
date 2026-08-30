886934256aad363972197700a7f6b4e7e336775e
Amruth S <kit28.24bec009@gmail.com>
2026-08-30 21:31:56 +0530

    ✅ Valid Anagram | Easy | Java | Runtime: 6 ms (43.64%) | Memory: 44.3 MB | 2026-08-30

--- leetcode/easy/0242-valid-anagram/solution.java
+++ leetcode/easy/0242-valid-anagram/solution.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())
-        return false;
+            return false;
         int[] arr=new int[26];
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<t.length();i++){
             arr[s.charAt(i)-'a']++;
             arr[t.charAt(i)-'a']--;
 
         }
         for(int n:arr){
-            if(n!=0){
+            if(n!=0)
                 return false;
-            }
-            
         }
         return true;
+        
         }
     }
