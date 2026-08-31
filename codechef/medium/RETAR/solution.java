4aa88993332863c1f1ee36b4beeb0d023769ad17
Amruth S <kit28.24bec009@gmail.com>
2026-08-31 20:06:07 +0530

    ✅ RETAR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/RETAR/solution.java
+++ codechef/medium/RETAR/solution.java
@@ -6,7 +6,14 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	    Scanner a=new Scanner(System.in);
+	    int g=(a.nextInt()*a.nextInt());
+	    int h=(a.nextInt()*a.nextInt());
+	    if(g+h>=a.nextInt())
+	    System.out.println("Yes");
+	    else
+	    System.out.println("No");
+		
 
 	}
 }
