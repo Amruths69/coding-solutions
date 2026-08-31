cf274385a2e34a2769b4ef98adf0d494c58e786a
Amruth S <kit28.24bec009@gmail.com>
2026-08-31 20:41:38 +0530

    ✅ SCOCN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.java
+++ codechef/medium/SCOCN/solution.java
@@ -6,7 +6,28 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner a=new Scanner(System.in);
+		int n=a.nextInt();
+		int[] arr=new int[n];
+		
+		for(int i=0;i<n;i++){
+		    arr[i]=a.nextInt();
+		}
+		int x=a.nextInt();
+		int c=0;
+		for(int i=0;i<n;i++){
+		    if(arr[i]==x){
+		        c++;
+		        if(c==2){
+		            System.out.println(i);
+		        }
+		    }
+		}
+		if(c==1){
+		    System.out.println(-2);
+		}else if(c==0){
+		    System.out.println(-1);
+		}
 
 	}
 }
