cd2e403901ec034f47ff3d3a9addcc489f369a4b
Amruth S <kit28.24bec009@gmail.com>
2026-09-02 22:05:40 +0530

    ✅ Valid Palindrome | Easy | Java | Runtime: 2 ms (99.30%) | Memory: 44 MB | 2026-09-02

--- leetcode/easy/0125-valid-palindrome/solution.java
+++ leetcode/easy/0125-valid-palindrome/solution.java
@@ -1,32 +1,20 @@
 ////daily-dump
 class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0;
-        int r = s.length() - 1;
-
-        while (l < r) {
-
-            // Skip non-alphanumeric characters from the left
-            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
-                l++;
-            }
-
-            // Skip non-alphanumeric characters from the right
-            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
-                r--;
-            }
-
-            char left = Character.toLowerCase(s.charAt(l));
-            char right = Character.toLowerCase(s.charAt(r));
-
-            if (left != right) {
-                return false;
-            }
-
+       int l=0;
+       int r=s.length()-1;
+       while(l<r){
+        while(l<r && !Character.isLetterOrDigit(s.charAt(l))){
             l++;
+        }while(l<r && !Character.isLetterOrDigit(s.charAt(r))){
             r--;
         }
-
-        return true;
+        if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+            return false;
+        }
+        l++;
+        r--;
+       }
+       return true;
     }
 }
