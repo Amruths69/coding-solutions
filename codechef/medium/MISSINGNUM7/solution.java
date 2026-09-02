38586c65f6f0fa1e1f0ebbbd1f403be26170fc7a
Amruth S <kit28.24bec009@gmail.com>
2026-09-02 21:21:57 +0530

    ✅ MISSINGNUM7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MISSINGNUM7/solution.java
+++ codechef/medium/MISSINGNUM7/solution.java
@@ -6,7 +6,9 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	    
+	    Scanner a=new Scanner(System.in);
+	    int s=a.nextInt();
+	    System.out.println(10-s);
 
 	}
 }
