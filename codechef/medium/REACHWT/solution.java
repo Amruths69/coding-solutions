c89bdb0c67eef5223403a151174383a4350f489e
Amruth S <kit28.24bec009@gmail.com>
2026-09-02 21:27:20 +0530

    ✅ REACHWT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/REACHWT/solution.java
+++ codechef/medium/REACHWT/solution.java
@@ -6,11 +6,7 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner a=new Scanner(System.in);
-		int t=a.nextInt();
-		while(t-->0){
-		    int n=a.nextInt();
-		}
+		// your code goes here
 
 	}
 }
