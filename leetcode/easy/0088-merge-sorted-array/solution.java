012d0da085dadeacfbf89b6b466b75830ba86ae8
Amruth S <kit28.24bec009@gmail.com>
2026-09-06 15:20:31 +0530

    ✅ Merge Sorted Array | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.8 MB | 2026-09-06

--- leetcode/easy/0088-merge-sorted-array/solution.java
+++ leetcode/easy/0088-merge-sorted-array/solution.java
@@ -1,31 +1,18 @@
-//daily-dump
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-
-        while (i >= 0 && j >= 0) {
-
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
-            } else {
-                nums1[k] = nums2[j];
-                j--;
+        int n1=m-1;
+        int n2=n-1;
+        int i=m+n-1;
+        while(n2>=0){
+            if(n1>=0&&nums1[n1]>nums2[n2]){
+                nums1[i]=nums1[n1];
+                n1--;
+            }else{
+                nums1[i]=nums2[n2];
+                n2--;
             }
-
-            k--;
+            i--;
         }
-
-        while (j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
-        }
+        
     }
 }
-        
-        
-        
