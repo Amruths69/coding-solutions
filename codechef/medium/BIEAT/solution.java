2ef83689e3d5d6b7fe241dca0baa1fb2c3a647ab
Amruth S <kit28.24bec009@gmail.com>
2026-09-07 20:28:26 +0530

    ✅ BIEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/BIEAT/solution.java
+++ codechef/medium/BIEAT/solution.java
@@ -6,7 +6,21 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		
+		Scanner a=new Scanner(System.in);
+		int n=a.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=a.nextInt();
+		    
+		}
+		int m=a.nextInt();
+		int g=(int)Math.pow(2,m);
+		int[] ans=new int[n];
+		for(int i=0;i<n;i++){
+		    ans[i]=arr[i]/g;
+		    
+		}
+		System.out.println(Arrays.toString(ans));
 
 	}
 }
