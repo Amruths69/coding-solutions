b3c72883b3767b6c9ce4d5d1c20a451b47c2d0a5
Amruth S <kit28.24bec009@gmail.com>
2026-09-07 20:08:07 +0530

    ✅ SAEVI | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/SAEVI/solution.java
+++ codechef/medium/SAEVI/solution.java
@@ -6,29 +6,7 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner a=new Scanner(System.in);
-		int n=a.nextInt();
-		int k=a.nextInt();
-		int g=2*k;
-		int[] arr=new int[n];
-		for(int i=0;i<n;i++){
-		    arr[i]=a.nextInt();
-		    
-		}
-		int s=0;
-		for(int i=0;i<n;i+=2){
-		    if(i==0||i%2==0){
-		        if(arr[i]>g)
-		        s++;
-		        
-		        
-		    }
-		}
-		if(s>0){
-		    System.out.println(s);
-		}else{
-		    System.out.println(0);
-		}
+		// your code goes here
 
 	}
 }
