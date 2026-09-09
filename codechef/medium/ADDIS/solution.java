32a46d8fd4a1793ebbc9f22770638fcc4b72fb5b
Amruth S <kit28.24bec009@gmail.com>
2026-09-09 20:23:17 +0530

    ✅ ADDIS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/ADDIS/solution.java
+++ codechef/medium/ADDIS/solution.java
@@ -6,7 +6,20 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		
+		Scanner a=new Scanner(System.in);
+		int t=a.nextInt();
+		while(t-->0){
+		    int n=a.nextInt();
+		    int[] arr=new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i]=a.nextInt();
+		    }
+		    int[] brr=new int[n];
+		    for(int i=0;i<n;i++){
+		        brr[i]=arr[i]+1;
+		    }
+		    
+		}
 
 	}
 }
