aec978e5d22de24cd6069aefdf8de64dd4db7c1f
Amruth S <kit28.24bec009@gmail.com>
2026-09-09 20:15:00 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.java
+++ codechef/medium/UNQSHF/solution.java
@@ -6,7 +6,32 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner a=new Scanner(System.in);
+		int t=a.nextInt();
+		while(t-->0){
+		    int n=a.nextInt();
+		    String aa=a.next();
+		    String bb=a.next();
+		    int ca=0;
+		    int cb=0;
+		    for(int i=0;i<n;i++){
+		        char g=aa.charAt(i);
+		        if(g=='a'){
+		            ca++;
+		        }
+		    }
+		    for(int i=0;i<n;i++){
+		        char g=bb.charAt(i);
+		        if(g=='b'){
+		            cb++;
+		        }
+		    }
+		    if(ca==cb){
+		        System.out.println("Yes");
+		    }else{
+		        System.out.println("No");
+		    }
+		}
 
 	}
 }
