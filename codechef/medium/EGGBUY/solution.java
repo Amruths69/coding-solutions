59601b09f0316bfd04b6de2b79c13a6752f03686
Amruth S <kit28.24bec009@gmail.com>
2026-09-09 20:05:43 +0530

    ✅ EGGBUY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/EGGBUY/solution.java
+++ codechef/medium/EGGBUY/solution.java
@@ -6,7 +6,17 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	    Scanner a=new Scanner(System.in);
+	    int x=a.nextInt();
+	    int y=a.nextInt();
+	    int f=a.nextInt();
+	    int h=x*12;
+	    int g=(y*12)+f;
+	    if(h>g){
+	        System.out.println(g);
+	    }else{
+	        System.out.println(h);
+	    }
 
 	}
 }
