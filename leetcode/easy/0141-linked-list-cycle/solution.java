fa228f0f6b03df429644c99420d1e6fcce4d393a
Amruth S <kit28.24bec009@gmail.com>
2026-09-09 05:40:50 +0530

    ✅ Linked List Cycle | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 47 MB | 2026-09-09

---
+++ leetcode/easy/0141-linked-list-cycle/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
+        ListNode f=head.next;
+        ListNode s=head;
+        while(s!=null && f!=null && f.next!=null){
+            if(f==s){
+                return true;
+            }
+            s=s.next;
+            f=f.next.next;
+
+        }
+        return false;
+        
+    }
+}
