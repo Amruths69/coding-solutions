ac5f97ce2e65a0f86b3187dec6f3c9a72559a25b
Amruth S <kit28.24bec009@gmail.com>
2026-09-12 05:53:54 +0530

    ✅ Valid Parentheses | Easy | Java | Runtime: 4 ms (36.28%) | Memory: 43.2 MB | 2026-09-12

--- leetcode/easy/0020-valid-parentheses/solution.java
+++ leetcode/easy/0020-valid-parentheses/solution.java
@@ -1,30 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        char[] stack = new char[s.length()];
-        int top = -1;
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (c == '(' || c == '{' || c == '[') {
-                stack[++top] = c;
-            } else {
-
-                // if stack is empty
-                if (top == -1) {
-                    return false;
-                }
-
-                char popped = stack[top--];
-
-                if ((c == ')' && popped != '(') ||
-                    (c == ']' && popped != '[') ||
-                    (c == '}' && popped != '{')) {
-                    return false;
-                }
+        HashMap<Character,Character> hm=new HashMap<>();
+        hm.put(')','(');
+        hm.put('}','{');
+        hm.put(']','[');
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(!hm.containsKey(c)){
+                st.push(c);
+            }else{
+                if(st.empty())
+                return false;
+                char te=st.pop();
+            if(te!=hm.get(c)){
+                return false;
             }
+            }
+            
         }
-
-        return top == -1;
+        
+        return st.isEmpty();
+        
     }
 }
