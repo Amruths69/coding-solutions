8b9c5ea155b0ee8a9416981ecccdf24e7c4eb00a
Amruth S <kit28.24bec009@gmail.com>
2026-09-21 20:04:07 +0530

    ✅ MADNER | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MADNER/solution.java
+++ codechef/medium/MADNER/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public int findMaximumPairs(String s) {
-        
+        Scanner a=new Scanner(System.in);
+        String b=a.next();
         int c=0;
-        for(int i=0;i<s.length()-1;i++){
-            char r=s.charAt(i);
-            char g=s.charAt(i+1);
-            if((r == 'x' && g == 'y') || (r == 'y' && g == 'x')){
+        for(int i=0;i<b.length()-1;i++){
+            char r=b.charAt(i);
+            char g=b.charAt(i+1);
+            if(r=='x'||r=='y'&&g=='x'||g=='y'){
                 c++;
-                i++;
+                r++;
             }else{
                 continue;
             }
         }
-       return c;
+        System.out.println(c);
     }
 }
